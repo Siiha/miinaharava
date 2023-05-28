62d16a86c4d581c60bb9871e74b9cecd115e5881
Siiha <90766306+Siiha@users.noreply.github.com>
2023-05-28 18:28:11 +0300

    Add files via upload

--- miinaharava.py
+++ miinaharava.py
@@ -32,9 +32,15 @@ def numeroi(sijainti,miinat,s):
 	for i in set(miinat):
 		c += i in v
 	return c
-s = 10
+def pum(katsotut,miinat):
+	for i in katsotut:
+		if i in miinat:
+			return True
+	return False
+			
+s = 5
 land = [[0 for i in range(s)] for i in range(s)]
-m = s*s//3
+m = s*s//5
 # miinat[0][1] 0 on y ja 1 on x
 miinat = [(random.randint(0,s-1),random.randint(0,s-1)) for i in range(m)]
 for i in miinat:
@@ -46,18 +52,29 @@ katsotut = []
 merkityt = []
 while set(merkityt) != set(miinat):
 	valinta = int(input("valinta miinamerkintä ta katso ruutu 0 tai 1: "))
-	syote = tuple(map(int,input("Anna koordinaatti muodossa y x: ").split()))
+	syote = tuple(map(int,input("Anna 0-"+str(s-1)+" koordinaatti muodossa y x: ").split()))
 	if valinta:
-		katsotut.append(syote)
-		if syote in miinat:
-			print("Pum")
-			break
+		if syote not in katsotut:
+			katsotut.append(syote)
 	else:
 		merkityt.append(syote)
 	kuva = [["X" for i in range(s)] for i in range(s)]
-	for i in katsotut:
-		kuva[i[0]][i[1]]=land[i[0]][i[1]]
 	for i in merkityt:
 		kuva[i[0]][i[1]]="M"
+	if valinta:
+		viimeisin = syote
+		if land[viimeisin[0]][viimeisin[1]]==0 or numeroi(viimeisin,merkityt,s) == land[viimeisin[0]][viimeisin[1]]:
+			vv = [i for i in viereiset(viimeisin,s) if i not in merkityt]
+			for i in vv:
+				if land[i[0]][i[1]]==0 or numeroi(i,merkityt,s)==land[i[0]][i[1]]:
+					vv+=[j for j in viereiset(i,s) if j not in vv and j not in merkityt]
+				
+			katsotut += vv
+	
+	for i in katsotut:
+		kuva[i[0]][i[1]]=str(land[i[0]][i[1]])
 	for i in kuva:
 		print(i)
+	if pum(katsotut,miinat):
+		print("Pum")
+		break
