1cdc16ac36de4db41a414019dc42b5f6b6d0b5f2
Siiha <90766306+Siiha@users.noreply.github.com>
2023-05-29 11:05:58 +0300

    Update miinaharava.py

--- miinaharava.py
+++ miinaharava.py
@@ -1,23 +1,5 @@
 import random
-def viereiset(sijainti,s):
-	x0, y0 = sijainti
-	a = (1, 0, -1)
-	for dx in a:
-		for dy in a:
-			if dx == 0 and dy == 0:
-				continue
-			x = x0 + dx
-			y = y0 + dy
-			if 0 <= x < s and 0 <= y < s:
-				yield (x, y)
-def numeroi(sijainti,miinat,s):
-	c = 0
-	v = set(viereiset(sijainti,s))
-	for i in set(miinat):
-		c += i in v
-	return c
-pum = lambda katsotut,miinat: set(katsotut) & set(miinat)
-			
+from miina_k import *
 s = int(input("Anna kentän sivun pituus: "))
 land = [[0 for i in range(s)] for i in range(s)]
 m = int(input("Anna haluamasi miinojen määrä (määrä voi olla pienempi, jos miinoille osuu sama sijainti ne lasketaan yhdeksi): "))
